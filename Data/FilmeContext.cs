b313fd06eaf4e1581dc9f240acfa93b5259f204e
JJBrito <jzjunior@gmail.com>
2023-09-06 12:16:17 -0300

    primeiro commit

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts) 
+    {
+    
+    }
+
+
+    public DbSet<Filme> Filmes { get; set; }
+}
---
+++ Profiles/FilmeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MoviesAPI.Data.Dtos;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile() 
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+}
